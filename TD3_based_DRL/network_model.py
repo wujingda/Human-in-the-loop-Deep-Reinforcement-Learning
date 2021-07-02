007166f941f112c282399a3a518f2fa783e94445
wujingda <37899467+wujingda@users.noreply.github.com>
2021-07-02 20:02:53 +0800

    Update network_model.py

--- TD3_based_DRL/network_model.py
+++ TD3_based_DRL/network_model.py
@@ -12,43 +12,33 @@ torch.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-BATCH_SIZE = 128
-
-def fanin_init(size,fanin=None):
-    fanin = fanin or size[0]
-    v = 1./ np.sqrt(fanin)
-    
-    return torch.Tensor(size).uniform_(-v,v)
-def he_init(tensor):
-    torch.nn.init.kaiming_uniform_(tensor, a=0, mode='fan_in', nonlinearity='leaky_relu')    
-
+### This actor network is established on CNN structure for the image-style state with the shape of 80*45.
 class Actor(nn.Module):
-    def __init__(self,nb_states,nb_actions,hidden=32,init_w=3e-1):
+    def __init__(self, nb_states, nb_actions, hidden=32, init_w=3e-1):
         super(Actor,self).__init__()
-        self.conv1 = nn.Conv2d(1,6,6)
-        self.conv2 = nn.Conv2d(6,16,6)
-        self.fc1 = nn.Linear(16*16*7,256)
-        self.fc2 = nn.Linear(256,128)
-        self.fc3 = nn.Linear(128,64)
-        self.fc4 = nn.Linear(64,nb_actions)
+        self.conv1 = nn.Conv2d(1, 6, 6)
+        self.conv2 = nn.Conv2d(6, 16, 6)
+        self.fc1 = nn.Linear(16*16*7, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, nb_actions)
         self.relu = nn.ReLU()
         self.sig = nn.Tanh()
         self.init_weights(init_w)
         
     def init_weights(self,init_w):
-        he_init( self.fc1.weight.data )
-        he_init( self.fc2.weight.data )
-        he_init( self.fc3.weight.data )
 
+        torch.nn.init.kaiming_uniform_(self.fc1.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')    
+        torch.nn.init.kaiming_uniform_(self.fc2.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.kaiming_uniform_(self.fc3.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
         self.fc4.weight.data.uniform_(-init_w,init_w)
         
     def forward(self,inp):
         
         x = inp.unsqueeze(1)
-
-        x = F.max_pool2d( self.conv1(x),2)
-        x = F.max_pool2d( self.conv2(x),2)
-        x = x.view(x.size(0),16*16*7)
+        x = F.max_pool2d( self.conv1(x), 2)
+        x = F.max_pool2d( self.conv2(x), 2)
+        x = x.view(x.size(0), 16*16*7)
         
         x = self.fc1(x)
         x = self.relu(x)
@@ -59,10 +49,12 @@ class Actor(nn.Module):
         x = self.fc4(x)
         out = self.sig(x)
         return out
-    
+
+### This critic network is established on MLP structure.
 class Critic(nn.Module):
-    def __init__(self,nb_states,nb_actions,hidden=256,init_w=3e-1):
+    def __init__(self, nb_states, nb_actions, hidden=256, init_w=3e-1):
         super(Critic,self).__init__()
+
         self.fc1 = nn.Linear(nb_states,hidden)
         self.fc2 = nn.Linear(hidden+nb_actions,hidden)
         self.fc3 = nn.Linear(hidden,hidden)
@@ -76,23 +68,25 @@ class Critic(nn.Module):
         self.fc51 = nn.Linear(hidden,1)
         
         self.relu = nn.ReLU()
-        self.sig = nn.Tanh()
+
         self.init_weights(init_w)
         
     def init_weights(self,init_w):
-        he_init( self.fc1.weight.data )
-        he_init( self.fc2.weight.data )
-        he_init( self.fc3.weight.data )
-        he_init( self.fc4.weight.data )
+
+        torch.nn.init.kaiming_uniform_(self.fc1.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.kaiming_uniform_(self.fc2.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.kaiming_uniform_(self.fc3.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.kaiming_uniform_(self.fc4.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
         self.fc5.weight.data.uniform_(-init_w,init_w)
         
-        he_init( self.fc11.weight.data )
-        he_init( self.fc21.weight.data )
-        he_init( self.fc31.weight.data )
-        he_init( self.fc41.weight.data )
+        torch.nn.init.kaiming_uniform_(self.fc11.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.kaiming_uniform_(self.fc21.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.kaiming_uniform_(self.fc31.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.kaiming_uniform_(self.fc41.weight.data, a=0, mode='fan_in', nonlinearity='leaky_relu')
         self.fc51.weight.data.uniform_(-init_w,init_w)
         
     def forward(self,inp):
+
         x,a = inp
         x = x.view(x.size(0),45*80)
         q1 = self.fc1(x)
